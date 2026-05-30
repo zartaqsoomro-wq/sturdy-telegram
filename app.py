0f0cf63b8bea5a69f704408f30bdb525f9fe385d
Maham Taqi Soomro <mahamtaqi@example.com>
2026-05-31 02:23:24 +0500

    Fix import path for backend

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
